c7acda4bff4bad42377fc13fd9ff9587a72b9fb5
Daniil Popov <popovdanin@gmail.com>
2024-08-21 16:29:44 +0300

    initial commit

---
+++ src/test/java/org/skyhigh/msnamingserver/MsNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyhigh.msnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
